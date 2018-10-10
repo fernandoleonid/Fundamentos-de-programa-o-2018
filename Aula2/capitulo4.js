737c83c86983a7fda4984025ad88020cabea8227
unknown <fernandoleonid@gmail.com>
2018-10-10 11:28:28 -0300

    aula dia 10/10/2018

--- Aula2/capitulo4.js
+++ Aula2/capitulo4.js
@@ -1,6 +1,8 @@
 var calcularA = document.getElementById("calcularA");
+var btnCalcularC = document.getElementById('calcularC');
 
-calcularA.addEventListener("click",exercicioA)
+calcularA.addEventListener("click",exercicioA);
+btnCalcularC.addEventListener('click',exercicioC);
 
 function exercicioA(){
 
@@ -15,5 +17,34 @@ function exercicioA(){
 		resultado.value = n2 - n1;
 		resultado.style.backgroundColor = "blue";
 	}
+
+}
+
+function exercicioC(){
+	
+	//Declarando variáveis
+	//Entrada
+	var nota1 = parseInt(document.getElementById('nota1').value);
+	var nota2 = parseInt(document.getElementById('nota2').value);
+	var nota3 = parseInt(document.getElementById('nota3').value);
+	var nota4 = parseInt(document.getElementById('nota4').value);
+	var txtMedia = document.getElementById('media');
+	var txtSituacao = document.getElementById('situacao');
+
+	//processamento
+	var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+	if (media >= 6){
+		var situacao = "Aprovado";
+		var cor = "green"
+	}else{
+		var situacao = "Reprovado";
+		var cor = "red"
+	}
+
+	//saída
+	txtMedia.value = media;
+	txtSituacao.value = situacao;
+	txtSituacao.style.backgroundColor = cor;
 
 }
